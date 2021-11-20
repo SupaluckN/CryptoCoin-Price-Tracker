6533ae364fda3b38b6cc232b68ba5991f68eb6ec
Supaluck Singjan <supalucksingjan@gmail.com>
2021-11-21 01:08:26 +0700

    initai

---
+++ src/components/Cryptolist.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import EZReacttable from 'ez-react-table'
+
+const Cryptolist = () => {
+    const [coins, setCoins] = useState([]);
+
+    const fetchCoins = async () => {
+        try {
+            const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+            const data = await res.json();
+            setCoins(data);
+        } catch (e) {
+            alert("Somting is wrong!!!")
+        }
+    };
+
+    useEffect(() => {
+        fetchCoins();
+    }, []);
+
+    const cols = [
+        {
+            title: "Name",
+            key: "name",
+            width: 200,
+            render: (value, object) => {
+                return (
+                    <div style={{ display: " flex", alignItems: "center" }} >
+                        <img
+                            height="20px"
+                            width="20px"
+                            src={object?.image}
+                            alt="coin" />
+                        <span style={{ marginLeft: "10px" }}>{object?.name}</span>
+                    </div >
+                )
+            }
+        },
+        {
+            title: "Price",
+            key: "current_price",
+            width: 100,
+            render: (value, object) => <div>{`$${value}`}</div>
+        },
+        {
+            title: "Change",
+            key: "price_change_percentage_24h",
+            width: 100,
+            render: (value, object) => {
+                return (
+                    <div style={{ color: /-/.test(value) ? "#ff0374" : "#06a847" }}>
+                        {value}%
+                    </div>
+                )
+            }
+        }
+    ]
+
+    return (
+
+        <EZReacttable
+            cols={cols}
+            data={coins}
+            darkMode
+            title="Crypto Tracker"
+            defaultSort="name"
+            accentColor="#ffbc03"
+            tableHeight={300}
+        />
+    )
+}
+
+export default Cryptolist
